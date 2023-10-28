f36911b7eeb89418bb105be7b4b24dd194922da9
lxRbckl <lxrbckl@gmail.com>
2023-10-28 13:16:06 -0500

    building interrelationships

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // < Project PasCam 8 by Alex Arbuckle > //
 
-const {fileSet} = require('lxrbckl');
+// import <
+
+
+// >
+
 (async () => {
 
-   await fileSet({
-
-      pData : "this is a test",
-      pFile : "test.txt"
-
-   })
+   //
 
 })();
 
